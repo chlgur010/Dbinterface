b286b82ab249eebec5b65826288092fa8771609b
hu-09 <hu-09@hu-09-PC>
2020-06-18 15:06:21 +0900

    CRUD 마무리

--- src/main/java/org/edu/controller/HomeController.java
+++ src/main/java/org/edu/controller/HomeController.java
@@ -16,6 +16,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Handles requests for the application home page.
@@ -27,6 +29,37 @@ public class HomeController {
 	
 	@Inject
 	private IF_SampleService sampleService;
+	
+	/**
+	 * 회원수정를 위한 경로, home,jsp같이사용
+	 * @throws Exception 
+	 */
+	@RequestMapping(value = "/admin/member/update", method = RequestMethod.POST)
+	public String updateMember( MemberVO memberVO, Model model, RedirectAttributes rdat) throws Exception {
+		sampleService.updateMember(memberVO);
+		return"redirect:/";//수정 된 후 화면 리플레쉬 하는 효과
+	}	
+	
+	/**
+	 * 회원삭제를 위한 경로, home,jsp같이사용
+	 * @throws Exception 
+	 */
+	@RequestMapping(value = "/admin/member/delete", method = RequestMethod.POST)
+	public String deleteMember(@RequestParam("userid") String userid, Model model, RedirectAttributes rdat) throws Exception {
+		sampleService.deleteMember(userid);
+		return"redirect:/";//삭제 된 후 화면 리플레쉬 하는 효과
+	}	
+	
+	/**
+	 * 회원등록을 위한 경로, home,jsp같이사용
+	 * @throws Exception 
+	 */
+	@RequestMapping(value = "/admin/member/create", method = RequestMethod.POST)
+	public String creatMember(MemberVO memberVO, Model model, RedirectAttributes rdat) throws Exception {
+		sampleService.insertMember(memberVO);
+		return"redirect:/";
+	}	
+	
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -42,7 +75,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		String maker = "김일국";
+		String maker = "최혁";
 		model.addAttribute("jspMaker", maker);
 		//제네릭타입-List<Type>을 사용하는 이유는 실행시(runtime)에서만 발생될 수 있는 에러를
 		//서버에 올리기 전에 미리 이클립스 단에서 컴파일시 에러를 확인 가능하게 하기 위해서 
